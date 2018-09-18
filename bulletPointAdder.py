0bb9b57477ae58c2d7df14adf674930942183803
e <43266378+jlf6@users.noreply.github.com>
2018-09-18 19:44:07 +0800

    在wiki标记中添加无序列表
    《python编程快速上手》第六章 字符串操作的项目

---
+++ bulletPointAdder.py
@@ -0,0 +1,9 @@
+#! python
+import pyperclip
+text = pyperclip.paste()
+
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+text = '\n'.join(lines)
+pyperclip.copy(text)
